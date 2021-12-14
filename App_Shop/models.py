f671c3d6f463d495925fab844da2a8cc3a2969eb
majid <majid.asadollahi95@gmail.com>
2021-12-14 15:04:26 +0330

    Product Details Page , Cart and Order Model

--- App_Shop/models.py
+++ App_Shop/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 
 
-
-
 class Category(models.Model):
     title = models.CharField(max_length=30)
     created = models.DateTimeField(auto_now_add=True)
--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     
     'App_Login',
     'App_Shop',
+    'App_Order',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
