5b5c101b6b788fc15c82e996edb4faa088aba67c
majid <majid.asadollahi95@gmail.com>
2021-12-20 00:09:32 +0330

    Increase and Decrease Quantity

--- App_Order/urls.py
+++ App_Order/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import add_to_cart, card_view
+from .views import add_to_cart, card_view, remove_from_cart
 
 
 app_name = 'App_Order'
@@ -8,6 +8,7 @@ app_name = 'App_Order'
 urlpatterns = [
     path('add/<pk>/', add_to_cart, name='add'),
     path('cart/', card_view, name='cart'),
+    path('remove/<pk>/', remove_from_cart, name='remove'),
 
 ]
 
